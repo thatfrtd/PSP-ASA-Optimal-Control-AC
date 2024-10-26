1c835e9c2d4f0772b03f8f51b067e86c6f5cd1c3
Lucas Mohler <dieplover@gmail.com>
2024-10-26 15:39:32 -0500

    Started Implementing State function
    Created a copy of Trajectory Optimizer that implements the state function. Also, vehicles without specified moments of inertia have their MOI estimated.

--- Vehicles/Vehicle.m
+++ Vehicles/Vehicle.m
@@ -6,23 +6,38 @@ classdef Vehicle
         m (1, 1) double
         L (1, 1) double
         I (1, 1) double
+        Len (1,1) double
         Name (1,1) string
     end
     
     methods
-        function obj = Vehicle(m, L, I, Name)
+        function obj = Vehicle(m, L, Len, options)
             %VEHICLE Construct an instance of this class
             %   Detailed explanation goes here
+            arguments
+                m
+                L
+                Len
+                options.I = Vehicle.estimateMOI(m, Len, L)
+                options.Name =  "vehicle"
+            end
             obj.m = m;
             obj.L = L;
-            obj.I = I;
-            obj.Name = Name;
+            obj.I = options.I;
+            obj.Len = Len;
+            obj.Name = options.Name;
         end
 
         function save(obj)
             vehicle = obj;
             path = string("Vehicles\" + obj.Name + ".mat");
             save(path, "vehicle");
+        end
+    end
+
+    methods(Static)
+        function I = estimateMOI(m, Len, L)
+            I = (1/3) * m * (Len)^2 + m * (L)^2; 
         end
     end
 end
